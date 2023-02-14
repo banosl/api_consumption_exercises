0ac4e80cd139a350a20bfce75bd7efd061d1ad90
Leo Banos Garcia <bsleo625@gmail.com>
2023-02-13 19:09:07 -0700

    Adds chuck norris service

---
+++ api_consumption/lib/chuck_service.rb
@@ -0,0 +1,33 @@
+require 'faraday'
+require 'json'
+
+class ChuckService
+
+  def self.conn
+    Faraday.new("https://api.chucknorris.io")
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body)
+  end
+
+  def self.random
+    response = conn.get('/jokes/random')
+    parse_json(response)
+  end
+
+  def self.random_in_category(category)
+    response = conn.get("/jokes/random?category=#{category}")
+    parse_json(response)
+  end
+
+  def self.categories
+    response = conn.get('/jokes/categories')
+    parse_json(response)
+  end
+
+  def self.search(query)
+    response = conn.get("/jokes/search?query=#{query}")
+    parse_json(response)
+  end
+end
